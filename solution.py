77b3b5695594f58e6fe7f6eed303103488be1a3e
allmaity <79119594+allmaity@users.noreply.github.com>
2021-09-25 23:42:04 -0400

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,59 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  #Fill in start
+  serverSocket.listen(1)
+  #Fill in end
+
+  while True:
+    #Establish the connection
+    #print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #server begins listening for incoming tcp requests #Fill in start      #Fill in end
+    try:
+
+      try:
+        message = connectionSocket.recv(1024) # recieves data in 1024 byte-sized packets #Fill in start    #Fill in end
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read() #reads file 'f' #Fill in start     #Fill in end
+        
+        #Send one HTTP header line into socket.
+        #Fill in start
+        headerForOK="\nHTTP/1.1 200 OK \n\n"
+        connectionSocket.send(headerForOK.encode())
+        #Fill in end
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
+
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+      except IOError:
+        # Send response message for file not found (404)
+        #Fill in start
+          headerForNotFound="\nHTTP/1.1 404 Not Found\n\n"
+          connectionSocket.send(headerForNotFound.encode())
+        #Fill in end
+
+
+        #Close client socket
+        #Fill in start
+          connectionSocket.close()
+        #Fill in end
+
+    except (ConnectionResetError, BrokenPipeError):
+      pass
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+  webServer(13331)
